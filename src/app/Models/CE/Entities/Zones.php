feb622fce3e82598ddb847d40df5b5e1191fef22
Ndifreke Ekott <ndy40.ekott@gmail.com>
2016-02-06 19:44:47 +0000

    allowed vagrant to connect to db from anywhere

---
+++ src/app/Models/CE/Entities/Zones.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ndy40
+ * Date: 06/02/16
+ * Time: 19:28
+ */
+
+namespace CE\Entities;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Zones
+ *
+ * @package CE\Entities
+ */
+class Zones extends Model
+{
+
+}
